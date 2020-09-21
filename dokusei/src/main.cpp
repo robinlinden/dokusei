fea3b28924f041d9fb1bb724956d7dbe11248b7d
Robin Linden <dev@robinlinden.eu>
2020-09-21 22:40:40 +0200

    Actually create and delete Tox instances when requested to

--- dokusei/src/main.cpp
+++ dokusei/src/main.cpp
@@ -4,18 +4,48 @@
 #include <proto/tox.grpc.pb.h>
 #include <tox/tox.h>
 
-#include <cstdlib>
-#include <ctime>
+#include <array>
+#include <cstdint>
 #include <iostream>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
 
 namespace {
+struct ToxDeleter {
+    void operator()(Tox *const t) {
+        tox_kill(t);
+    }
+};
+
+struct ToxOptionsDeleter {
+    void operator()(Tox_Options *const o) {
+        tox_options_free(o);
+    }
+};
+
 class ToxService final : public ToxAPI::Service {
+public:
     grpc::Status Create(
             grpc::ServerContext *context,
             const CreateRequest *request,
             CreateResponse *response) override {
-        const auto id = std::rand(); // NOLINT(cert-msc50-cpp): This is very temporary.
-        std::cout << "Created Tox instance " << id << "." << std::endl;
+        std::unique_ptr<Tox_Options, ToxOptionsDeleter> opts{tox_options_new(nullptr), ToxOptionsDeleter{}};
+        if (!opts) return grpc::Status::CANCELLED;
+
+        std::unique_ptr<Tox, ToxDeleter> tox{tox_new(opts.get(), nullptr), ToxDeleter{}};
+        if (!tox) return grpc::Status::CANCELLED;
+
+        std::array<uint8_t, TOX_ADDRESS_SIZE> addr{};
+        tox_self_get_address(tox.get(), addr.data());
+
+        toxii.push_back(std::move(tox));
+
+        const auto id = std::accumulate(begin(addr), end(addr), 0);
+        std::cout << "Created Tox instance " << id << "." << '\n';
+        std::cout << "Now have " << toxii.size() << " toxii." << std::endl;
         response->set_id(id);
         return grpc::Status::OK;
     }
@@ -24,15 +54,30 @@ class ToxService final : public ToxAPI::Service {
             grpc::ServerContext *context,
             const DeleteRequest *request,
             DeleteResponse *response) override {
-        std::cout << "Destroyed Tox instance " << request->id() << "." << std::endl;
+        auto tox = std::find_if(begin(toxii), end(toxii), [&](const auto &t) {
+            std::array<uint8_t, TOX_ADDRESS_SIZE> addr{};
+            tox_self_get_address(t.get(), addr.data());
+            const auto id = std::accumulate(begin(addr), end(addr), 0);
+            return id == request->id();
+        });
+
+        if (tox == end(toxii)) {
+            std::cout << "There is no Tox instance " << request->id() << " to destroy." << std::endl;
+            return grpc::Status::CANCELLED;
+        }
+
+        toxii.erase(tox);
+        std::cout << "Destroyed Tox instance " << request->id() << "." << '\n';
+        std::cout << "Now have " << toxii.size() << " toxii." << std::endl;
         return grpc::Status::OK;
     }
+
+private:
+    std::vector<std::unique_ptr<Tox, ToxDeleter>> toxii;
 };
 } // namespace
 
 int main() {
-    std::srand(std::time(nullptr)); // NOLINT(cert-msc51-cpp): This is very temporary.
-
     std::cout << "Linked against Tox "
             << tox_version_major() << "."
             << tox_version_minor() << "."
